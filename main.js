48eab896e5e946ee46568600a49465ea23c156b4
OGURA DAIKI <dkog2001@gmail.com>
2022-11-03 01:03:51 +0900

    テンプレートのインデント修正

--- main.js
+++ main.js
@@ -128,200 +128,200 @@ class TaskTimer extends LitElement {
   }
   addDialog() {
     return html`
-        <div class="backdrop centering">
-          <div class="dialog col gap-0">
-            <div class="row bar">
-              <div class="title grow">
-                タスクを追加
-              </div>
-              <button @click=${e => {
-                this.openAddDialog = false;
-              }}>×</button>
+      <div class="backdrop centering">
+        <div class="dialog col gap-0">
+          <div class="row bar">
+            <div class="title grow">
+              タスクを追加
             </div>
-            <div class="col grow content" style="overflow-y:scroll">
-              <div class="row">
-                <input
-                  type="text"
-                  class="grow"
-                  id="inputName"
-                  placeholder="タイトル"
-                  value=${this.inputName}
-                  @input=${e => {
-                    this.inputName = e.target.value
-                    this.#saveTasks();
-                  }}
-                  @keydown=${e => { if (e.key === "Enter" && e.ctrlKey) this.addTask(); }}
-                >
-                <button
-                  @click=${e => {
-                    this.inputName = "";
-                    this.renderRoot.querySelector("#inputName").value = "";
-                  }}
-                >
-                  ×
-                </button>
-              </div>
-              <div class="row grow">
-                <textarea
-                  id="inputMemo"
-                  class="grow noresize"
-                  placeholder="メモ"
-                  @input=${e => {
-                    this.inputMemo = e.target.value;
-                    this.#saveTasks();
-                  }}
-                  @keydown=${e => { if (e.key === "Enter" && e.ctrlKey) this.addTask(); }}
-                >${this.inputMemo}</textarea>
-                <button
-                  @click=${e => {
-                    this.inputMemo = "";
-                    this.renderRoot.querySelector("#inputMemo").value = "";
-                  }}
-                >
-                  ×
-                </button>
-              </div>
-              <button @click=${e => { this.addTask(); }}>追加</button>
+            <button @click=${e => {
+              this.openAddDialog = false;
+            }}>×</button>
+          </div>
+          <div class="col grow content" style="overflow-y:scroll">
+            <div class="row">
+              <input
+                type="text"
+                class="grow"
+                id="inputName"
+                placeholder="タイトル"
+                value=${this.inputName}
+                @input=${e => {
+                  this.inputName = e.target.value
+                  this.#saveTasks();
+                }}
+                @keydown=${e => { if (e.key === "Enter" && e.ctrlKey) this.addTask(); }}
+              >
+              <button
+                @click=${e => {
+                  this.inputName = "";
+                  this.renderRoot.querySelector("#inputName").value = "";
+                }}
+              >
+                ×
+              </button>
             </div>
+            <div class="row grow">
+              <textarea
+                id="inputMemo"
+                class="grow noresize"
+                placeholder="メモ"
+                @input=${e => {
+                  this.inputMemo = e.target.value;
+                  this.#saveTasks();
+                }}
+                @keydown=${e => { if (e.key === "Enter" && e.ctrlKey) this.addTask(); }}
+              >${this.inputMemo}</textarea>
+              <button
+                @click=${e => {
+                  this.inputMemo = "";
+                  this.renderRoot.querySelector("#inputMemo").value = "";
+                }}
+              >
+                ×
+              </button>
+            </div>
+            <button @click=${e => { this.addTask(); }}>追加</button>
           </div>
         </div>
-      `;
+      </div>
+    `;
   }
   copyDialog() {
     return html`
-        <div class="backdrop centering">
-          <div class="dialog col gap-0">
-            <div class="row bar">
-              <div class="title grow">
-                タスク時系列コピー
-              </div>
-              <button @click=${e => this.openCopyDialog = false}>×</button>
+      <div class="backdrop centering">
+        <div class="dialog col gap-0">
+          <div class="row bar">
+            <div class="title grow">
+              タスク時系列コピー
             </div>
-            <div class="col grow content" style="overflow-y:scroll">
-              <textarea class="noresize grow">${this.tasks.map(task => {
-                  return `${task.getTimeStr("from")}～${task.getTimeStr("to")} ${task.name}${task.memo ? `\n${task.memo.split("\n").map(s => "  " + s).join("\n")}` : ""}`;
-                }).reverse().join("\n")
-              }</textarea>
-            </div>
+            <button @click=${e => this.openCopyDialog = false}>×</button>
+          </div>
+          <div class="col grow content" style="overflow-y:scroll">
+            <textarea class="noresize grow">${this.tasks.map(task => {
+                return `${task.getTimeStr("from")}～${task.getTimeStr("to")} ${task.name}${task.memo ? `\n${task.memo.split("\n").map(s => "  " + s).join("\n")}` : ""}`;
+              }).reverse().join("\n")
+            }</textarea>
           </div>
         </div>
-      `;
+      </div>
+    `;
   }
   timeInput(task, name) {
     console.log(task, name, task.getTimeStr(name))
     return html`
-      <input
-        type="time" value="${task.getTimeStr(name)}"
-        @input=${e => {
-          task.setTimeStr(name, e.target.value);
-          this.#saveTasks();
-        }}
-      >
-      `
+    <input
+      type="time" value="${task.getTimeStr(name)}"
+      @input=${e => {
+        task.setTimeStr(name, e.target.value);
+        this.#saveTasks();
+      }}
+    >
+    `
   }
   taskView(task, i) {
     return html`
-        <div class="col task-item">
-          <input 
-            type="text" .value=${task.name}
-            @input=${e => {
-        task.name = e.target.value;
-      }} style="font-size:1.2em"
-          >
-          <textarea @input=${e => task.memo = e.target.value}>${task.memo}</textarea>
-          <div class="row">
-            ${this.timeInput(task, "from")}
-            <span>～</span>
-            ${task.to ? this.timeInput(task, "to") : "実施中"}
-          </div>
-          
-          <div class="row">
-            <button
-              ?disabled=${task.to}
-              @click=${e => {
-                task.to = Date.now();
-                this.requestUpdate();
-              }}
-            >
-              完了
-            </button>
-            <button
-              ?disabled=${!task.to}
-              @click=${e => {
-        if (confirm(`タスク名：${task.name} の完了を取り消しますか？`)) {
-          task.to = undefined;
-          this.requestUpdate();
-        }
-      }}
-            >
-              未完了
-            </button>
-            <button
-              @click=${e => {
-        this.inputName = task.name;
-        this.inputMemo = task.memo;
-        this.openAddDialog = true;
-      }}
-            >複製</button>
-            <button
-              @click=${e => {
-        if (confirm(`タスク名：${task.name}を削除しますか？`)) {
-          const idx = this.tasks.findIndex(t => t === task);
-          this.tasks.splice(idx, 1);
-          this.requestUpdate();
-        }
-      }}
-            >削除</button>
-          </div>
+      <div class="col task-item">
+        <input 
+          type="text" .value=${task.name}
+          @input=${e => {
+            task.name = e.target.value;
+          }} style="font-size:1.2em"
+        >
+        <textarea @input=${e => task.memo = e.target.value}>${task.memo}</textarea>
+        <div class="row">
+          ${this.timeInput(task, "from")}
+          <span>～</span>
+          ${task.to ? this.timeInput(task, "to") : "実施中"}
         </div>
-        <button @click=${e => {
+        
+        <div class="row">
+          <button
+            ?disabled=${task.to}
+            @click=${e => {
+              task.to = Date.now();
+              this.requestUpdate();
+            }}
+          >
+            完了
+          </button>
+          <button
+            ?disabled=${!task.to}
+            @click=${e => {
+              if (confirm(`タスク名：${task.name} の完了を取り消しますか？`)) {
+                task.to = undefined;
+                this.requestUpdate();
+              }
+            }}
+          >
+            未完了
+          </button>
+          <button
+            @click=${e => {
+              this.inputName = task.name;
+              this.inputMemo = task.memo;
+              this.openAddDialog = true;
+            }}
+          >複製</button>
+          <button
+            @click=${e => {
+              if (confirm(`タスク名：${task.name}を削除しますか？`)) {
+                const idx = this.tasks.findIndex(t => t === task);
+                this.tasks.splice(idx, 1);
+                this.requestUpdate();
+              }
+            }}
+          >削除</button>
+        </div>
+      </div>
+      <button @click=${e => {
         this.insertTo = i + 1;
         this.openAddDialog = true;
       }}>＋</button>
-      `;
+    `;
   }
   render() {
     this.#saveTasks();
     return html`
-        <div class="col gap-0 main-content">
-          <span class="pageTitle">打刻（簡易）</span>
-          <div class="grow col task_list">
-            <button @click=${e => {
-        this.insertTo = 0;
-        this.openAddDialog = true;
-      }}>＋</button>
-            ${repeat(this.tasks, t => t.id, (t, i) => this.taskView(t, i))}
-          </div>
-          <div class="row menu">
-            <button @click=${e => {
-        this.insertTo = 0;
-        this.openAddDialog = true;
-      }}>追加</button>
-            <button @click=${e => {
-        this.insertTo = 0;
-        this.inputName = "トイレ";
-        this.openAddDialog = true;
-      }}>トイレ</button>
-            <button @click=${e => {
-        this.insertTo = 0;
-        this.inputName = "相談MTG";
-        this.openAddDialog = true;
-      }}>相談MTG</button>
-            <button @click=${e => this.openCopyDialog = true}>
-              書き出し
-            </button>
-            <button @click=${e => {
-        if (confirm("入力されているタスクを全て削除します。本当によろしいですか？")) {
-          this.tasks = [];
-        }
-      }}>
-              クリア
-            </button>
-          </div>
+      <div class="col gap-0 main-content">
+        <span class="pageTitle">打刻（簡易）</span>
+        <div class="grow col task_list">
+          <button @click=${e => {
+            this.insertTo = 0;
+            this.openAddDialog = true;
+          }}>＋</button>
+          ${repeat(this.tasks, t => t.id, (t, i) => this.taskView(t, i))}
         </div>
-        ${(this.openAddDialog ? () => this.addDialog() : () => "")()}
-        ${(this.openCopyDialog ? () => this.copyDialog() : () => "")()}
-      `;
+        <div class="row menu">
+          <button @click=${e => {
+            this.insertTo = 0;
+            this.openAddDialog = true;
+          }}>追加</button>
+          <button @click=${e => {
+            this.insertTo = 0;
+            this.inputName = "トイレ";
+            this.openAddDialog = true;
+          }}>トイレ</button>
+          <button @click=${e => {
+            this.insertTo = 0;
+            this.inputName = "相談MTG";
+            this.openAddDialog = true;
+          }}>相談MTG</button>
+          <button @click=${e => this.openCopyDialog = true}>
+            書き出し
+          </button>
+          <button @click=${e => {
+            if (confirm("入力されているタスクを全て削除します。本当によろしいですか？")) {
+              this.tasks = [];
+            }
+          }}>
+            クリア
+          </button>
+        </div>
+      </div>
+      ${(this.openAddDialog ? () => this.addDialog() : () => "")()}
+      ${(this.openCopyDialog ? () => this.copyDialog() : () => "")()}
+    `;
   }
   updated() {
     this.renderRoot.querySelector("#inputName")?.focus();
